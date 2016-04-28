195271a4e3a5244c2b3b470d75160c7b11b5fe7a
ShivonQ <realitykeeper@gmail.com>
2016-04-28 14:18:41 -0500

    First Commit.
                  TODO:
                  Dynamic Month Gen-->Dynamic Year Gen
                      -form for adding events & Holidays
                  Npc Page
                      -form for NPC info
                  City Page--> Finish
                      -Make it make Blank NPCs and Shops
                      -Make the shops follow the shop rules for D&D
                            -Graph of income for shops
                  Character Bio Page
                      -Form for editing Bio
                      -figure out good formatting for it
                      -image input
    
                  OTHERS AS WELL

--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,43 @@ router.get('/map', function(req,res,next){
 });
 
 module.exports = router;
+
+/*
+/*var express = require('express');
+ var router = express.Router();
+ var passport=require('passport');
+ /* GET home/login page. */
+/*
+router.get('/', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/signup', function(req,res,next){
+  res.render('signup',{message:req.flash('signupMessage') } )
+});
+
+router.post('/signup',passport.authenticate('local-signup',{
+  successRedirect:'/tasks',
+  failureRedirect:'/signup',
+  failureFlash:true
+}));
+router.post('/login',passport.authenticate('local-login',{
+  successRedirect:'/tasks',
+  failureRedirect:'/',
+  failureFlash:true
+}));
+//get the logout 'page'
+router.get('/logout', function(req,res,next){
+  req.logout();
+  res.redirect('/');
+});
+
+//function for are they logged in?!!?!? not ogged in = back to homepage.
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/');
+}
+module.exports = router;
+*/
